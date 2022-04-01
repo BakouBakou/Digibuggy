2ce5082e893758b8767675332b2e908d53b566d4
Ruben Lommelen <ruben.lommelen@gmail.com>
2022-04-01 09:57:35 +0200

    6A

---
+++ src/main/java/com/switchfully/digibuggy/members/MemberController.java
@@ -0,0 +1,9 @@
+package com.switchfully.digibuggy.members;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/members")
+public class MemberController {
+}
---
+++ src/test/java/com/switchfully/digibuggy/members/MemberControllerTest.java
@@ -0,0 +1,17 @@
+package com.switchfully.digibuggy.members;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class MemberControllerTest {
+
+    @LocalServerPort
+
+
+    //GIVEN
+    //WHEN
+    //THEN
+}
