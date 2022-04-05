1d497bfd9b414867ea9ac41302f798ac26b177fa
Alperen2112 <a.alperen@outlook.com>
2022-04-05 13:42:05 +0200

    9) added tests for security

---
+++ src/main/java/com/switchfully/digibuggy/security/exception/UnauthorizatedException.java
@@ -0,0 +1,8 @@
+package com.switchfully.digibuggy.security.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class UnauthorizatedException extends RuntimeException {
+}
