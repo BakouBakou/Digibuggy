00dff72f1af846c26242ce6dccf69b42f06f927a
Alperen2112 <a.alperen@outlook.com>
2022-04-05 10:56:33 +0200

    9) added quick and dirty implemenation for create librarian test

--- src/main/java/com/switchfully/digibuggy/users/librarians/Librarian.java
+++ src/main/java/com/switchfully/digibuggy/users/librarians/Librarian.java
@@ -1,4 +1,34 @@
 package com.switchfully.digibuggy.users.librarians;
 
+import java.util.UUID;
+
 public class Librarian {
+
+    private final String id;
+    private final String lastName;
+    private final String firstName;
+    private final String emailAddress;
+
+    public Librarian(String firstName, String lastName, String emailAddress) {
+        this.id = UUID.randomUUID().toString();
+        this.firstName = firstName;
+        this.emailAddress = emailAddress;
+        this.lastName = lastName;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
 }
