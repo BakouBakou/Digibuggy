ade5af9484652ea53aa64b38119934b6bd97372d
StefanieVloemans <stefanievloemans@gmail.com>
2022-04-05 10:44:07 +0200

    story 11 - bullet point 2

--- src/main/java/com/switchfully/digibuggy/books/LendABook.java
+++ src/main/java/com/switchfully/digibuggy/books/LendABook.java
@@ -6,6 +6,7 @@ public class LendABook {
     private final String memberId;
     private final String isbn;
     private final String lendingId;
+    private final LocalDate dueDate;
 //    private LocalDate date;
 
 
@@ -13,10 +14,11 @@ public class LendABook {
         this.memberId = memberId;
         this.isbn = isbn;
         this.lendingId = UUID.randomUUID().toString();
+        this.dueDate = LocalDate.now().plusWeeks(3);
     }
 
 
     public String getLendingId() {
-        return this.lendingId;
+        return lendingId;
     }
 }
---
+++ src/test/java/com/switchfully/digibuggy/books/LendABookUnitTest.java
@@ -0,0 +1,18 @@
+package com.switchfully.digibuggy.books;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class LendABookUnitTest {
+
+    @Test
+    public void givenNewLendABook_ThenDueDateIsTodayPlus3Weeks() {
+        //GIVEN
+        LendABook lendABook = new LendABook("AAAA", "BBBB");
+
+        //THEN
+        Assertions.assertThat(lendABook.getDueDate()).isEqualTo(LocalDate.now().plusWeeks(3));
+    }
+}
