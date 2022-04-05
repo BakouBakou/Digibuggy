61942e4d9ac5e6d251bad277971ee09a28cf463a
Alperen2112 <a.alperen@outlook.com>
2022-04-05 13:41:59 +0200

    9) added tests for security

--- src/test/java/com/switchfully/digibuggy/users/librarians/LibrarianTest.java
+++ src/test/java/com/switchfully/digibuggy/users/librarians/LibrarianTest.java
@@ -3,6 +3,7 @@ package com.switchfully.digibuggy.users.librarians;
 import com.switchfully.digibuggy.users.librarians.dtos.LibrarianDto;
 import com.switchfully.digibuggy.users.librarians.dtos.RegisterLibrarianDto;
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,6 +26,9 @@ public class LibrarianTest {
 
         LibrarianDto librarianDto = RestAssured
                 .given()
+                    .auth()
+                    .preemptive()
+                    .basic("Bugs", "Bunny")
                     .body(registerLibrarianDto)
                     .accept(JSON)
                     .contentType(JSON)
@@ -41,6 +45,69 @@ public class LibrarianTest {
         Assertions.assertThat(librarianDto.getLastName()).isEqualTo(registerLibrarianDto.getLastName());
         Assertions.assertThat(librarianDto.getEmailAddress()).isEqualTo(registerLibrarianDto.getEmailAddress());
 
+    }
+
+    @Test
+    void givenWrongPassword_whenCreatingLibrarian_ThenExceptionIsThrwon() {
+        RegisterLibrarianDto registerLibrarianDto = new RegisterLibrarianDto("Bugs", "Bunny", "bugs@bunny.com");
+
+        Response librarianDto = RestAssured
+                .given()
+                .auth()
+                .preemptive()
+                .basic("Bugs", "Bun")
+                .body(registerLibrarianDto)
+                .accept(JSON)
+                .contentType(JSON)
+                .when()
+                .port(port)
+                .post("/admin/librarians")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.FORBIDDEN.value())
+                .extract()
+                .response();
+    }
+
+    @Test
+    void givenWrongUserName_whenCreatingLibrarian_ThenExceptionIsThrwon() {
+        RegisterLibrarianDto registerLibrarianDto = new RegisterLibrarianDto("Bugs", "Bunny", "bugs@bunny.com");
+
+        Response librarianDto = RestAssured
+                .given()
+                .auth()
+                .preemptive()
+                .basic("Kamiel", "De Kat")
+                .body(registerLibrarianDto)
+                .accept(JSON)
+                .contentType(JSON)
+                .when()
+                .port(port)
+                .post("/admin/librarians")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.FORBIDDEN.value())
+                .extract()
+                .response();
+    }
+
+    @Test
+    void givenNoAuthorizationInHeader_whenCreatingLibrarian_ThenExceptionIsThrwon() {
+        RegisterLibrarianDto registerLibrarianDto = new RegisterLibrarianDto("Bugs", "Bunny", "bugs@bunny.com");
+
+        Response librarianDto = RestAssured
+                .given()
+                .body(registerLibrarianDto)
+                .accept(JSON)
+                .contentType(JSON)
+                .when()
+                .port(port)
+                .post("/admin/librarians")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.FORBIDDEN.value())
+                .extract()
+                .response();
     }
 
 
